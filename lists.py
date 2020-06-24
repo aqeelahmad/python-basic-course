f00982cdadfedf1915c2f20e9500bd107e42546d
Aqeel Ahmad <aqeel.ahmad.se@gmail.com>
2020-06-24 21:26:18 +0400

    remove too many blank lines

--- lists.py
+++ lists.py
@@ -8,7 +8,6 @@ gives you second last element of the list.
 
 my_list = [1, 2, 3, 4, 5]
 
-
 # Slicing
 # Syntex list[start:end]
 # We use : to slice a list, by supplying no value after the colon, we tell Python
@@ -18,7 +17,6 @@ my_list = [1, 2, 3, 4, 5]
 
 print(my_list[2:])
 # Output: [3, 4, 5]
-
 
 # Example 2: Getting everything until the 3rd index
 # We just need to leave the starting part empty and have 3 in the ending part
@@ -30,7 +28,6 @@ print(my_list[:3])
 
 print(my_list[:-1])
 # Output: [1, 2, 3, 4]
-
 
 # One note that a slice creates a complete new list with no references
 # to the original list. Which means if you store slice in a variable
